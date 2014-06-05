86cf4ca552908815125d2468b990115010af70f1
sue445 <sue445@sue445.net>
2014-06-05 21:30:48 +0900

    Add rspec3 style spec

--- spec/lib/sample_spec.rb
+++ spec/lib/sample_spec.rb
@@ -5,8 +5,19 @@ describe Sample do
   let(:sample){ Sample.new }
 
   describe "#say" do
-    subject { sample.say(greeting) }
-    let(:greeting){ "Hello" }
-    it { should eq "Hello TDD BootCamp!" }
+    describe "Using expect to" do
+      let(:greeting){ "Hello" }
+
+      it "should say 'Hello TDD BootCamp!'" do
+        expect(sample.say(greeting)).to eq "Hello TDD BootCamp!"
+      end
+    end
+
+    describe "Using subject it should " do
+      subject { sample.say(greeting) }
+
+      let(:greeting){ "Yeah!" }
+      it { should eq "Yeah! TDD BootCamp!" }
+    end
   end
 end
